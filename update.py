433c49302d04ba073c583b58fceb6e86fdc7dd6b
ean <bchaney@nmu.edu>
2020-07-28 10:36:02 -0400

    Update python complete

--- update.py
+++ update.py
@@ -1,19 +1,10 @@
-import urllib
-from urllib.request import urlopen
-import gitPython
+import git
 
 def update():
-    update = False
-
-    versionSource = open('version.txt', 'r')
-    versionContents = versionSource.read()
-
-    updateSource = urlopen("https://github.com/cyberneticsLab/NMU_EQProject/blob/master/update.txt")
-    updateContents = updateSource.read()
-
-    print(updateContents)
-
-    if versionContents != updateContents: print('Outdated')
+    
+    my_repo = git.Repo()
+    if my_repo.is_dirty(untracked_files=True):
+        print('Changes detected.')
 
 
 update()
